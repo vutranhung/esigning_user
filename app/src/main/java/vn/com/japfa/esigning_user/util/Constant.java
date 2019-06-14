c7717c3598e1c28a685d4f70f27fe4a304c46745
vutranhung <vutranhung2000@gmail.com>
2019-06-14 14:59:36 +0700

    change to call restful api

---
+++ app/src/main/java/vn/com/japfa/esigning_user/util/Constant.java
@@ -0,0 +1,12 @@
+package vn.com.japfa.esigning_user.util;
+
+public class Constant {
+    public static String SERVICE_URL_PROPERTY="Service_Url";
+    public static String SERVICE_URL_VALUE="";
+    public  static  String APP_NAME_PROPERTY="APP_NAME";
+    public  static  String APP_NAME_VALUE="";
+
+    public  static  String DOWNLOAD_FILE_URL_PROPERTY="Download_File_Url";
+    public  static  String DOWNLOAD_FILE_URL_VALUE="";
+
+}
---
+++ app/src/main/java/vn/com/japfa/esigning_user/util/UtilHelper.java
@@ -0,0 +1,62 @@
+package vn.com.japfa.esigning_user.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class UtilHelper {
+    public static boolean checkInternetConnection(Activity activity) {
+
+        ConnectivityManager connManager =
+                (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+
+        if (networkInfo == null) {
+            Toast.makeText(activity, "No default network is currently active", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (!networkInfo.isConnected()) {
+            Toast.makeText(activity, "Network is not connected", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (!networkInfo.isAvailable()) {
+            Toast.makeText(activity, "Network not available", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public static Properties loadPropertyFile(Context context) throws IOException {
+        Properties properties = new Properties();
+        AssetManager assetManager = context.getAssets();
+        InputStream inputStream = assetManager.open("config.properties");
+        properties.load(inputStream);
+        return properties;
+    }
+
+    public static void getValueProperty(Context context)
+    {
+        try{
+            Properties properties = loadPropertyFile(context);
+            Constant.SERVICE_URL_VALUE = properties.getProperty(Constant.SERVICE_URL_PROPERTY);
+            Constant.APP_NAME_VALUE=properties.getProperty(Constant.APP_NAME_PROPERTY);
+            Constant.DOWNLOAD_FILE_URL_VALUE=properties.getProperty(Constant.DOWNLOAD_FILE_URL_PROPERTY);
+        }catch (Exception ex){
+            Toast.makeText(context, "Không đọc được file cấu hình config.properties", Toast.LENGTH_LONG);
+        }
+    }
+
+
+}
